b2ed89957f1fb6cf5fd2ff24c2c1627ebc5da28e
Helder Falcão <helder@ciandt.com>
2020-04-07 15:39:41 -0300

    Initial REST structure

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,27 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/institution">Institution</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+
+import Vue from "vue";
+import { MdButton, MdField, MdSnackbar } from "vue-material/dist/components";
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdField)
+Vue.use(MdSnackbar)
+
+export default {
+  
+}
+</script>
 
 <style>
 #app {
---
+++ src/views/InstitutionSave.vue
@@ -0,0 +1,51 @@
+<template>
+  <form novalidate @submit.stop.prevent="showSnackbar = true">
+    <div>
+      <md-field>
+        <label>Nome</label>
+        <md-input v-model="name"></md-input>
+      </md-field>
+
+      <md-field>
+        <label>Descrição</label>
+        <md-input v-model="description"></md-input>
+      </md-field>
+
+      <md-field>
+        <label>Responsável</label>
+        <md-input v-model="responsible"></md-input>
+      </md-field>
+      <md-button type="submit" class="md-fab md-primary" v-on:click="save()">
+        <md-icon>add</md-icon>
+      </md-button>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "InstitutionSave",
+  data: () => {
+    return {
+      showSnackbar: false,
+      instituicao: {
+        name: null,
+        description: null,
+        responsible: null,
+      },
+    };
+  },
+  methods: {
+    save () {
+      const instituicao = this.instituicao;
+      axios
+        .post("http://www.helderfalcao.com.br/instituicoes", instituicao)
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
+};
+</script>
